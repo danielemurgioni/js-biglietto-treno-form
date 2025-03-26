e25b6a7661230318a64a7605cd3def803964cb80
danielemurgioni <dashomew@gmail.com>
2025-03-26 18:38:21 +0100

    [UPD] added a randomNumber function and show the ticket values in html through javascript

--- js/script.js
+++ js/script.js
@@ -3,14 +3,42 @@
 let kilometers = document.getElementById("kilometers");
 let age = document.getElementById("age");
 let btnConfirm = document.getElementById("btn-confirm");
+let nameSurname = document.getElementById("name-surname");
+// DOM: risultato del form
+let passengerName = document.getElementById("passenger-name");
+let ticketType = document.getElementById("ticket-type");
+let wagonNumber = document.getElementById("wagon-number");
+let cpCode = document.getElementById("cp-code");
+let ticketPrice = document.getElementById("ticket-price");
 
+// invio i risultati del form all'utente
 btnConfirm.addEventListener("click", (event) => {
     // con preventDefault impedisco alla pagina l'auto-refresh cosi che io possa visualizare il risultato
     event.preventDefault();
     // prendo il valore quando clicco il bottone
     let valueKilometers = kilometers.value.trim();
     let valueAge = age.value.trim();
-    trainTicketCost(valueKilometers, valueAge);
+    let ticketCost = trainTicketCost(valueKilometers, valueAge);
+    let valueNameSurname = nameSurname.value.trim();
+    // visualizzazione su html:
+    //nome passeggero
+    passengerName.innerText = valueNameSurname;
+    //tipo di biglietto
+    if(valueAge<18){
+        ticketType.innerText = "Sconto del 20%"
+    }
+    else if(valueAge>65){
+        ticketType.innerText = "Sconto del 40%"
+    }
+    else{
+        ticketType.innerText = "Biglietto Standard"
+    }
+    //numero vagone
+    wagonNumber.innerText = randomNumber(1, 8);
+    //codice cp
+    cpCode.innerText = randomNumber(2000, 9999);
+    //costo biglietto
+    ticketPrice.innerText = `${ticketCost} €`;
 });
 
 //Function
@@ -29,5 +57,10 @@ function trainTicketCost(kilometers, age){
     }
 
     // con toFixed il valore number di ticketCost diventa string, ma arrotondo le cifre decimali del numero a 2
-    console.log(`${ticketCost.toFixed(2)} costo del biglietto`);
+    return ticketCost.toFixed(2);
+}
+
+function randomNumber(min, max){
+    let number = Math.floor(Math.random() * (max - min) + min);
+    return number;
 }
