a0ee6377c47c2707dea9c9616e4d39fca1391b7e
danielemurgioni <dashomew@gmail.com>
2025-03-26 15:52:04 +0100

    [UPD] click button confirm to get the result and [FEAT] turned ticket calculus into a function

--- js/script.js
+++ js/script.js
@@ -1,41 +1,3 @@
-/* calcolo del biglietto del treno
-
-// chiedo all'utente il numero di chilometri che vuole percorrere
-
-let kilometers = parseInt(prompt("Inserisci il numero di chilometri da percorrere"));
-
-console.log(kilometers)
-
-// chiedo all'utente l'età
-
-let age = parseInt(prompt("Inserisci la tua età"));
-
-console.log(age)
-
-// calcolo il prezzo del biglietto
-
-let ticketCost = 0.21 * kilometers;
-
-// applichiamo uno sconto al biglietto in base all'età
-let ticketDiscount = ticketCost;
-
-// se il passeggero è minorenne ha 20% di sconto
-if(age<18){
-    ticketDiscount *= 0.8;
-}
-// altrimenti se il passeggero è over 65 ha 40% di sconto
-else if(age>64){
-    ticketDiscount *= 0.6;
-}
-
-console.log(ticketCost)
-console.log(ticketDiscount)
-
-// con toFixed il valore number di ticketCost diventa string, ma arrotondo le cifre decimali del numero a 2
-console.log(`${ticketDiscount.toFixed(2)} costo del biglietto`);
-
-*/
-
 //connetto il DOM a javascript
 let inputKilometers = document.getElementById("kilometers");
 let inputAge = document.getElementById("age");
@@ -51,3 +13,28 @@ let age = inputAge.value;
 
 console.log(kilometers);
 console.log(age);
+
+btnConfirm.addEventListener("click", () => {
+    trainTicketCost(kilometers, age)
+});
+
+//Function
+function trainTicketCost(kilometers, age){
+    // calcolo il prezzo del biglietto
+    let ticketCost = 0.21 * kilometers;
+
+    // applichiamo uno sconto al biglietto in base all'età
+    let ticketDiscount = ticketCost;
+
+    // se il passeggero è minorenne ha 20% di sconto
+    if(age<18){
+        ticketDiscount *= 0.8;
+    }
+    // altrimenti se il passeggero è over 65 ha 40% di sconto
+    else if(age>64){
+        ticketDiscount *= 0.6;
+    }
+
+    // con toFixed il valore number di ticketCost diventa string, ma arrotondo le cifre decimali del numero a 2
+    console.log(`${ticketDiscount.toFixed(2)} costo del biglietto`);
+}
